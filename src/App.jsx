584968cd843c44160a7fe94fccd0609b13e14932
Pragati <pragatikhanna06@gmail.com>
2026-07-13 13:48:35 +0530

    updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,3 @@
-// import React from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// import HomePage from "./HomePage";
-// import ServicesPage from "./ServicesPage";
-// import ReportCrimePage from "./pages/ReportCrimePage";
-
-// import DataSecurityPage from "./pages/services/DataSecurityPage";
-// import ForensicAuditPage from "./pages/services/ForensicAuditPage";
-// import DigitalForensicsPage from "./pages/services/DigitalForensicsPage";
-// import FraudInvestigationPage from "./pages/services/FraudInvestigationPage";
-// import InvestigationsPage from "./pages/services/InvestigationsPage";
-// import LegalConsultationPage from "./pages/services/LegalConsultationPage";
-// import DocumentExaminationPage from "./pages/services/DocumentExaminationPage";
-
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/report-crime" element={<ReportCrimePage />} />
-//         <Route path="/services" element={<ServicesPage />} />
-//         <Route path="/services/data-security" element={<DataSecurityPage />} />
-//         <Route path="/services/forensic-audit" element={<ForensicAuditPage />} />
-//         <Route path="/services/digital-forensics" element={<DigitalForensicsPage />} />
-//         <Route path="/services/fraud-investigation" element={<FraudInvestigationPage />} />
-//         <Route path="/services/investigations" element={<InvestigationsPage />} />
-//         <Route path="/services/legal-consultation" element={<LegalConsultationPage />} />
-//         <Route path="/services/document-examination" element={<DocumentExaminationPage />} />
-//         <Route path="*" element={<HomePage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -95,7 +10,7 @@ import BookLawyerInBetweenPage from "./pages/BookLawyerInBetweenPage";
 import CaseStatusPage from "./pages/CaseStatusPage";
 import NyayShieldPage from "./pages/NyayShieldPage";
 import ForensicExpertPage from "./pages/ForensicExpertPage";
-import NearbyPoliceStationPage from "./pages/NearbyPoliceStationPage";
+import NearbyPoliceStationPage from "./pages/NearByPoliceStationPage";
 import LegalDraftingPage from "./pages/LegalDraftingPage";
 
 import DataSecurityPage from "./pages/services/DataSecurityPage";
@@ -118,7 +33,7 @@ export default function App() {
         <Route path="/book-lawyer/in-between" element={<BookLawyerInBetweenPage />} />
         <Route path="/case-status" element={<CaseStatusPage />} />
         <Route path="/forensic-expert" element={<ForensicExpertPage />} />
-        <Route path="/nearby-police-station" element={<NearbyPoliceStationPage />} />
+        <Route path="/nearby-police-station" element={<NearByPoliceStationPage />} />
         <Route path="/legal-drafting" element={<LegalDraftingPage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/services/data-security" element={<DataSecurityPage />} />
